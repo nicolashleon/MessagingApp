52b911e208ab8681ecdafd70e34994061163069b
Nicolas Hurtado <nicolashleon@users.noreply.github.com>
2019-02-11 16:13:10 -0500

    Fixed import to allow compilation on CircleCI

--- app/src/main/java/com/nicolashurtado/messagingapp/ui/activities/MessagingActivity.kt
+++ app/src/main/java/com/nicolashurtado/messagingapp/ui/activities/MessagingActivity.kt
@@ -3,7 +3,7 @@ package com.nicolashurtado.messagingapp.ui.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import com.nicolashurtado.messagingapp.BuildConfig
+import com.nicolashurtado.messagingapp.BuildConfig.DATA_SEED_FILE_NAME
 import com.nicolashurtado.messagingapp.R
 import com.nicolashurtado.messagingapp.ui.viewmodels.MessageViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -15,7 +15,7 @@ class MessagingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_messaging)
-        viewModel.loadData(BuildConfig.DATA_SEED_FILE_NAME)
+        viewModel.loadData(DATA_SEED_FILE_NAME)
         viewModel.getPublications().observe(this, Observer { publications ->
             print("MESSAGES $publications.size")
         })
